4726b4904b52c68fb715549ff3c492c55b7c0989
vesdev <28784043+vesdev@users.noreply.github.com>
2026-02-03 15:32:07 +0200

    cli loading spinner

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,6 @@
+use indicatif::{ProgressBar, ProgressStyle};
 use miette::Result;
-use std::path::PathBuf;
+use std::{path::PathBuf, time::Duration};
 
 use argh::FromArgs;
 
@@ -17,15 +18,26 @@ fn main() -> Result<()> {
     let args: Args = argh::from_env();
 
     if let Some(path) = args.path {
-        let src = std::fs::read_to_string(path).map_err(|e| miette::miette!(e.to_string()))?;
-        let result = kisu::run(&src);
-        println!("{result:?}");
+        let source = std::fs::read_to_string(path).map_err(|e| miette::miette!(e.to_string()))?;
+        run(&source);
     }
 
-    if let Some(code) = args.code {
-        let result = kisu::run(&code);
-        println!("{result:?}");
+    if let Some(source) = args.code {
+        run(&source);
     }
 
     Ok(())
+}
+
+fn run(source: &str) {
+    let pb = ProgressBar::new_spinner();
+    pb.enable_steady_tick(Duration::from_millis(120));
+    pb.set_style(
+        ProgressStyle::with_template("{spinner:.blue} {msg:.magenta}")
+            .unwrap()
+            .tick_strings(&["⢎ ", "⠎⠁", "⠊⠑", "⠈⠱", " ⡱", "⢀⡰", "⢄⡠", "⢆⡀", ""]),
+    );
+    pb.set_message("Running kisu...");
+    let result = kisu::run(source);
+    pb.finish_with_message(format!("{result:?}"));
 }
